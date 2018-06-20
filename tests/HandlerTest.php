98ae17bfeac89dd1f8cc89997a762506fdd3d65c
罗晓俊 <gogitservice@gmail.com>
2018-06-20 14:30:59 +0800

    unit test

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -1,3 +1,22 @@
 <?php
 
-//
+class HandlerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testLogSuccess()
+    {
+        $log_path = __DIR__ . '/../logs/test.log';
+        file_exists($log_path) && unlink($log_path);
+        $handler = new \Lxj\Monolog\Co\Stream\Handler($log_path);
+        $monolog = new \Monolog\Logger('test');
+        $monolog->pushHandler($handler);
+        $monolog->info('test log');
+
+        $now = date('Y-m-d H:i:s');
+
+        $expected = <<<EOF
+[{$now}] test.INFO: test log [] []
+
+EOF;
+        $this->assertStringEqualsFile($log_path, $expected);
+    }
+}
